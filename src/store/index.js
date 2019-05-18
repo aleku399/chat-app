3ed0bbd61cd67d9201df4b58d0cc12394145aec7
aleku399 <aleku399@gmail.com>
2019-05-18 17:13:35 +0300

    feat: setting up boiler plate

---
+++ src/store/index.js
@@ -0,0 +1,11 @@
+import { createStore } from "redux";
+import { contacts } from "../static-data";
+import reducer from "../reducers";
+
+const store = createStore(
+        reducer,
+        {contacts},
+        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    ); 
+
+export default store;
