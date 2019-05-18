41810116c577a153d1fc5a779e7224a954e50ce3
aleku399 <aleku399@gmail.com>
2019-05-18 18:21:22 +0300

    feat: add chat window and other components

--- src/containers/App.js
+++ src/containers/App.js
@@ -6,12 +6,12 @@ import Sidebar from "../components/Sidebar";
 import Main from "../components/Main";
 
 function App() {
-  const { contacts } = store.getState();
+  const { contacts, user, activeUserId  } = store.getState();
 
   return (
     <div className="App">
      <Sidebar contacts={_.values(contacts)}/>
-     <Main />
+     <Main user={user} activeUserId={activeUserId} />
     </div>
   );
 }
